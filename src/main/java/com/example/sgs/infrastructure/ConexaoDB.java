9984422096414fb37bfc50abee8038f94227cc17
naldo <naldo864159@gmail.com>
2026-05-10 21:55:36 -0300

    feat: Adiciona aplicação em ambiente contenerizado (docker)

--- src/main/java/com/example/sgs/infrastructure/ConexaoDB.java
+++ src/main/java/com/example/sgs/infrastructure/ConexaoDB.java
@@ -4,50 +4,15 @@ import java.sql.*;
 
 public class ConexaoDB {
 
-    final static String url = "jdbc:mysql://localhost:3306/sgs";
+    final static String url = "jdbc:mysql://mysql:3306/sgs";
     final static String user = "user";
     final static String password = "123456";
-
-    public static void teste() {
-        try {
-            Connection conn = conectar();
-            Statement stmt = conn.createStatement();
-            ResultSet resultSet = stmt.executeQuery("select * from teste");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("id"));
-                System.out.println(resultSet.getString("nome"));
-            }
-            conn.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static Connection conectar() {
-        // remover variaveis
-        String url = "jdbc:mysql://localhost:3306/sgs";
-        String user = "user";
-        String password = "123456";
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e) {
-            System.err.println("Falha: " + e.getMessage());
-            return null;
-        }
-    }
 
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
-            throw new RuntimeException("Falha ao tentar conexão com o banco de dados.");
+            throw new RuntimeException("Falha ao tentar conexão com o banco de dados.", e);
         }
     }
 }
